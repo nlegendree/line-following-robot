fbe0d70fb52479f9ba0fa3654cbae963845eea4b
Legendre Nathan <nathan.legendre@insa-rouen.fr>
2023-10-25 14:05:36 +0200

    Test lineFinder

---
+++ src/lineFinder.c
@@ -0,0 +1,27 @@
+#include <wiringPi.h>
+#include <stdio.h>
+
+#define LINE_FINDER_PIN 17 // Utilisez le numéro de la broche GPIO correct
+
+int main(void) {
+    if (wiringPiSetupGpio() == -1) {
+        printf("Échec de l'initialisation de wiringPi. Vérifiez vos privilèges.\n");
+        return 1;
+    }
+
+    pinMode(LINE_FINDER_PIN, INPUT);
+
+    while (1) {
+        int lineDetected = digitalRead(LINE_FINDER_PIN);
+
+        if (lineDetected == LOW) {
+            printf("Ligne détectée\n");
+        } else {
+            printf("Ligne non détectée\n");
+        }
+
+        delay(100); // Attendre 100 millisecondes avant de lire à nouveau
+    }
+
+    return 0;
+}
