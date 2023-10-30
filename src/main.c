1795d67ccec7014dd3a14c86555433bcdd061807
Thomas Bauer <thomas.bauer@insa-rouen.fr>
2023-10-30 14:43:52 +0100

    ajout LCD dans le main

--- src/main.c
+++ src/main.c
@@ -1,28 +1,36 @@
 #include "controller.h"
+#include "i2c_lcd.h"
 
+#include <libevdev/libevdev.h>
+#include <libevdev/libevdev-uinput.h>
+#include <wiringPi.h>
+#include <lcd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <string.h>
-#include <libevdev/libevdev.h>
-#include <libevdev/libevdev-uinput.h>
 
 int main() {
 
-    //Controller Initialization
+    // LCD Initialization
+    int lcd = initLCD();
+
+    // Controller Initialization
     struct libevdev *controller = initController();
     printf("Input device name : \"%s\"\n", libevdev_get_name(controller));
+    getchar();
 
-    //Controller Detection
+    // Controller Detection
+    struct input_event ev;
     int Xstate = 0;
     int L2state = 0;
     int R2state = 0;
     int LXstate = MID_AXIS;
     while (1) {
-        struct input_event ev;
-        libevdev_next_event(controller, LIBEVDEV_READ_FLAG_NORMAL, &ev);
         system("clear");
-
+        lcdClear(lcd);
+        
+        libevdev_next_event(controller, LIBEVDEV_READ_FLAG_NORMAL, &ev);
         if (buttonIsPressed(BUTTON_CROSS,controller,ev,&Xstate)) {
             printf("X is pressed\n");
         }
@@ -30,6 +38,7 @@ int main() {
         int L2 = triggerValue(TRIGGER_L2,controller,ev,&L2state);
         if (L2 > 0) {
             printf("L2 : %d\n",L2);
+            lcdPrintf(lcd,"L2 : %d\n",L2);
         }
 
         int R2 = triggerValue(TRIGGER_R2,controller,ev,&R2state);
@@ -41,7 +50,8 @@ int main() {
         if (LX != MID_AXIS) {
             printf("AXIS LEFT X : %d\n",LX);
         }
-        //delay(10);
+
+        delay(10);
     }
     
     return 0;
