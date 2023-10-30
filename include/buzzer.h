1656a06a18c5daa78f9e1fe7f2fa74182bc675cd
Thomas Bauer <thomas.bauer@insa-rouen.fr>
2023-10-30 13:39:38 +0100

    buzzer

---
+++ include/buzzer.h
@@ -0,0 +1,8 @@
+#ifndef __BUZZER__
+#define __BUZZER__
+
+
+void initBuzzer();
+
+
+#endif
--- src/buzzer.c
+++ src/buzzer.c
@@ -1,24 +1,8 @@
 #include "gpio_pin.h"
+#include "buzzer.h"
 
 #include <wiringPi.h>
-#include <stdio.h>
 
-int main() {
-    if (wiringPiSetupGpio() == -1) {
-        printf("Échec de l'initialisation de WiringPi. Vérifiez vos privilèges.\n");
-        return 1;
-    }
-
+void initBuzzer() {
     pinMode(PIN_BUZZER, OUTPUT);
-
-    while (1) {
-        // Activer le buzzer pour générer un signal sonore
-        digitalWrite(PIN_BUZZER, HIGH);
-        delay(1000); // Laisser le buzzer actif pendant 1 seconde
-        // Désactiver le buzzer
-        digitalWrite(PIN_BUZZER, LOW);
-        delay(1000); // Attendre pendant 1 seconde
-    }
-
-    return 0;
 }
