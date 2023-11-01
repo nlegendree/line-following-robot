79d68a1b3f6364e1530ef01116f6b0f5ec146cfa
angelo <angelo.bou_tanous@insa-rouen.fr>
2023-11-01 15:04:49 +0100

    modification programme suiveurDeLigne

--- src/suiveurDeLigne.c
+++ src/suiveurDeLigne.c
@@ -33,30 +33,15 @@ void suivreLigne(int pin_capteur_gauche, int pin_capteur_centre, int pin_capteur
     bool droite = detecterLigne(pin_capteur_droit);
     bool intersection = detecterIntersection(pin_capteur_gauche,pin_capteur_centre, pin_capteur_droit)
 
-    if (detecterIntersection(gauche,centre,droite))
+    if (detecterIntersection(gauche,centre,droite) || centre || !(gauche,centre,droite)) // si aucun capteur n'a detecte la ligne, on avance quand meme
         avancer();
-    else if (!gauche){
-        tournerDroite();
-    }
-    else if (!droite){
+    else if (gauche){
         tournerGauche();
     }
-    else if(intersection){
-        int direction = rand() % 3; // retourne un nombre aléatoire entre 0, 1, 2
-        if (direction == 0){
-            avancer();
-        }
-        else if(direction == 1) {
-            tournerGauche();
-        }
-        else{
-            tournerDroite();
-        }
+    else if (droite){
+        tournerDroite();
     }
-    else{
-        //rien n'est détecté, le root est perdu -> alerte, arret d'urgence et message
+    else if(detecterIntersectionEnT(gauche,centre,droite)){ // on considere que la fin du parcours est marquée par une intersection en T
         arreter();
-        buzzerOn();
-        lcdWrite("Aucune ligne detectée");
     }
 }
