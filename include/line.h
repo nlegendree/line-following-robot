b08e159269f22aa5513693161b8fe75d2c421a57
angelo <angelo.bou_tanous@insa-rouen.fr>
2023-11-01 14:58:26 +0100

    modification programme suiveurDeLigne

--- include/line.h
+++ include/line.h
@@ -1,6 +1,8 @@
 #ifndef __LINE__
 #define __LINE__
 
+void initSuiveurLigne()
 bool detecterLigne(int pin_linefinder);
 bool detecterIntersection(int pin_capteur_gauche, int pin_capteur_centre, int pin_capteur_droit);
+bool detecterIntersectionEnT(bool gauche, bool centre, bool droite)
 void suivreLigne(int pin_capteur_gauche, int pin_capteur_centre, int pin_capteur_droit);
