075426d6d2b277aadb1be22d0b8d6071c3e42a23
Thomas Bauer <thomas.bauer@insa-rouen.fr>
2023-11-01 14:05:13 +0100

    corr

--- include/gpio_pin.h
+++ include/gpio_pin.h
@@ -3,7 +3,9 @@
 
 
 #define PIN_BUZZER 1
-#define PIN_LINEFINDER 2
+#define PIN_SUIVEUR_GAUCHE 2
+#define PIN_SUIVEUR_CENTRE 3
+#define PIN_SUIVEUR_DROIT 4
 #define PIN_TRIG 17  // GPIO pin pour le signal Trig
 #define PIN_ECHO 18  // GPIO pin pour le signal Echo
 
--- src/suiveurDeLigne.c
+++ src/suiveurDeLigne.c
@@ -1,39 +1,30 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
 #include "moteur.h"
 #include "i2cLCD.h"
 #include "buzzer.h"
+#include "gpio_pin.h"
 
-bool detecterLigne(int pin_linefinder) {
-    if (wiringPiSetupGpio() == -1) {
-        prboolf("Échec de l'initialisation de wiringPi. Vérifiez vos privilèges.\n");
-        return false;
-    }
+#include <stdio.h>
+#include <stdlib.h>
 
-    pinMode(pin_linefinder, INPUT);
-    bool lineDetected = detecterLigne(pin_linefinder);
-
-    if (lineDetected == LOW) {
-        return true; //ligne détectée
-    } else {
-        return false; //ligne non détectée
-    }
+void initSuiveurLigne() {
+    pinMode(PIN_SUIVEUR_GAUCHE, INPUT);
+    pinMode(PIN_SUIVEUR_CENTRE, INPUT);
+    pinMode(PIN_SUIVEUR_DROIT, INPUT);
 }
 
-bool detecterIntersection(int pin_capteur_gauche, int pin_capteur_centre, int pin_capteur_droit){
-
-    bool gauche = detecterLigne(pin_capteur_gauche);
-    bool centre = detecterLigne(pin_capteur_centre);
-    bool droite = detecterLigne(pin_capteur_droit);
-
-    // Si les trois capteurs sont actifs alors on considère la présence d'une boolersection
-    if (gauche  && centre && droit) {
-        return true;
-    } else {
-        return false;
-    }
+bool detecterLigne(int pin_capteur) {
+    return digitalRead(pin_capteur);
 }
+
+bool detecterIntersection(bool gauche, bool centre, bool droite){
+    // Si les trois capteurs sont actifs alors on considère la présence d'une intersection
+    return (gauche && centre && droite) || (gauche && centre) || (centre && droite);
+}
+
+bool detecterIntersectionEnT(bool gauche, bool centre, bool droite){
+    return gauche && droite;
+}
+
 
 void suivreLigne(int pin_capteur_gauche, int pin_capteur_centre, int pin_capteur_droit){
     
@@ -42,7 +33,7 @@ void suivreLigne(int pin_capteur_gauche, int pin_capteur_centre, int pin_capteur
     bool droite = detecterLigne(pin_capteur_droit);
     bool intersection = detecterIntersection(pin_capteur_gauche,pin_capteur_centre, pin_capteur_droit)
 
-    if (gauche && centre && droit)
+    if (detecterIntersection(gauche,centre,droite))
         avancer();
     else if (!gauche){
         tournerDroite();
