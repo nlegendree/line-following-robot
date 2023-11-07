d9c38af6139a1676017ded0a49b6783929b6c64d
Thomas Bauer <thomas.bauer@insa-rouen.fr>
2023-11-07 22:57:14 +0100

    retour en arrière

--- src/main.c
+++ src/main.c
@@ -11,39 +11,6 @@
 #include <fcntl.h>
 #include <string.h>
 
-int firstLoop = TRUE;
-
-void manualControl() {
-    if (firstLoop) {
-        firstLoop = FALSE;
-
-        int Xstate = 0;
-        int L2state = 0;
-        int R2state = 0;
-        int LXstate = MID_AXIS;
-    }
-
-    if (buttonIsBeingPressed(BUTTON_SQUARE,controller,ev,&Xstate)) {
-        printf("SQUARE is pressed\n");
-    }
-
-    int L2 = triggerValue(TRIGGER_L2,controller,ev,&L2state);
-    if (L2 > 0) {
-        printf("L2 : %d\n",L2);
-        lcdPrintf(lcd,"L2 : %d\n",L2);
-    }
-
-    int R2 = triggerValue(TRIGGER_R2,controller,ev,&R2state);
-    if (R2 > 0) {
-        printf("R2 : %d\n",R2);
-    }
-
-    int LX = axisValue(AXIS_LX,controller,ev,&LXstate);
-    if (LX != MID_AXIS) {
-        printf("AXIS LEFT X : %d\n",LX);
-    }
-}
-
 int main() {
 
     // LCD Initialization
@@ -56,6 +23,13 @@ int main() {
     struct libevdev *controller = initController();
     printf("Input device name : \"%s\"\n", libevdev_get_name(controller));
     getchar();
+
+
+    // Event States Initialization
+    int Xstate = 0;
+    int L2state = 0;
+    int R2state = 0;
+    int LXstate = MID_AXIS;
 
     // Controller Detection
     struct input_event ev;
@@ -73,8 +47,26 @@ int main() {
         }
 
         // Manual Mode
-        manualControl();
-        
+        if (buttonIsBeingPressed(BUTTON_SQUARE,controller,ev,&Xstate)) {
+            printf("SQUARE is pressed\n");
+        }
+
+        int L2 = triggerValue(TRIGGER_L2,controller,ev,&L2state);
+        if (L2 > 0) {
+            printf("L2 : %d\n",L2);
+            lcdPrintf(lcd,"L2 : %d\n",L2);
+        }
+
+        int R2 = triggerValue(TRIGGER_R2,controller,ev,&R2state);
+        if (R2 > 0) {
+            printf("R2 : %d\n",R2);
+        }
+
+        int LX = axisValue(AXIS_LX,controller,ev,&LXstate);
+        if (LX != MID_AXIS) {
+            printf("AXIS LEFT X : %d\n",LX);
+        }
+
         delay(10);
     }
     
