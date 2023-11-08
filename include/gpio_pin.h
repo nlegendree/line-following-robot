c5dd90b248dd006c5f3adf0cc38c13c11f90788b
Thomas Bauer <thomas.bauer@insa-rouen.fr>
2023-11-08 14:03:24 +0100

    motors

--- include/gpio_pin.h
+++ include/gpio_pin.h
@@ -3,11 +3,18 @@
 
 
 #define PIN_BUZZER 1
+
 #define PIN_SUIVEUR_GAUCHE 2
 #define PIN_SUIVEUR_CENTRE 3
 #define PIN_SUIVEUR_DROIT 4
+
 #define PIN_TRIG 17  // GPIO pin pour le signal Trig
 #define PIN_ECHO 18  // GPIO pin pour le signal Echo
+
+#define PIN_M1A 18
+#define PIN_M1B 12
+#define PIN_M2A 13
+#define PIN_M2B 19
 
 
 #endif
--- src/motors.c
+++ src/motors.c
@@ -1,17 +1,13 @@
+#include "gpio_pin.h"
+
 #include <wiringPi.h>
 
-// Définir les numéros de broches GPIO utilisées pour le contrôle du moteur
-#define M1A 18  // Broche GPIO pour le contrôle du moteur (IN1)
-#define M1B 12 // Broche GPIO pour le contrôle du moteur (IN2)
-#define M2B 19  // Broche GPIO pour le contrôle du moteur (IN2)
-#define M2A 13  // Broche GPIO pour le contrôle du moteur (IN2)
-
 // Initialisation de la bibliothèque WiringPi
-void init() {
-    pinMode(M1A, PWM_OUTPUT);
-	pinMode(M1B, PWM_OUTPUT);
-	pinMode(M2A, PWM_OUTPUT);
-	pinMode(M2B, PWM_OUTPUT);
+void initMotors() {
+    pinMode(PIN_M1A, PWM_OUTPUT);
+	pinMode(PIN_M1B, PWM_OUTPUT);
+	pinMode(PIN_M2A, PWM_OUTPUT);
+	pinMode(PIN_M2B, PWM_OUTPUT);
     pwmSetMode(PWM_MODE_MS);
     pwmSetClock(400);    // Réglage de la fréquence PWM
     pwmSetRange(1024);   // Réglage de la plage PWM (0-1024)
@@ -25,6 +21,7 @@ void forward(int R2) {
     pwmWrite(M2A, (int)(R2*1024)/256);
 }
 
+/*
 // Fonction pour faire reculer le moteur
 void reverse() {
     digitalWrite(MOTOR_PIN1, LOW);
@@ -44,7 +41,7 @@ void turnRight(int angle) {
    
 
 }
-
+*/
 
 
 int main() {
