7ed635dc636b2ed0c9c84e6fafc88e2df1df2d7c
angelo <angelo.bou_tanous@insa-rouen.fr>
2023-12-13 12:34:38 +0100

    speaker.c

---
+++ code/src/speaker.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <wiringPi.h>
+#include <SDL2/SDL.h>
+
+#include "gpioPins.h"
+
+void initSpeaker() {
+    pinMode(PIN_SPEAKER, OUTPUT);
+}
+
+void playAudio(int button) {
+    const char *audioFiles[] = {"audio1.mp3", "audio2.mp3", "audio3.mp3", "audio4.mp3"};
+
+    // associe une valeur à la touche pressée par l'utilisateur
+    int arrowPressed = -1;
+    switch (button) {
+        case SDL_CONTROLLER_BUTTON_DPAD_UP:
+            arrowPressed = 1;
+            break;
+        case SDL_CONTROLLER_BUTTON_DPAD_DOWN:
+            arrowPressed = 2;
+            break;
+        case SDL_CONTROLLER_BUTTON_DPAD_LEFT:
+            arrowPressed = 3;
+            break;
+        case SDL_CONTROLLER_BUTTON_DPAD_RIGHT:
+            arrowPressed = 4;
+            break;
+        default:
+            return;
+    }
+    if (arrowPressed != -1){
+        const char command[100];
+        sprintf(command, "mpg123 %s", audioFiles[arrowPressed-1]);
+        system(command)
+    }
+}
