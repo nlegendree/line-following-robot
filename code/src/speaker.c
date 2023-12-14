e005682da8e0c8523a4e6caba621f6a35c182d89
Thomas Bauer <thomas.bauer@insa-rouen.fr>
2023-12-14 14:58:49 +0100

    correction code

--- code/src/speaker.c
+++ code/src/speaker.c
@@ -10,28 +10,25 @@ void initSpeaker() {
 }
 
 void playAudio(SDL_Event event) {
-    const char *audioFiles[] = {"audio1.mp3", "audio2.mp3", "audio3.mp3", "audio4.mp3"};
-
-    // associe une valeur à la touche pressée par l'utilisateur
-    int arrowPressed = -1;
+    int audioIndex = -1;
     if (event.cbutton.type == SDL_CONTROLLERBUTTONDOWN) {
         switch (event.cbutton.button) {
             case SDL_CONTROLLER_BUTTON_DPAD_UP:
-                arrowPressed = 0;
+                audioIndex = 0;
                 break;
             case SDL_CONTROLLER_BUTTON_DPAD_DOWN:
-                arrowPressed = 1;
+                audioIndex = 1;
                 break;
             case SDL_CONTROLLER_BUTTON_DPAD_LEFT:
-                arrowPressed = 2;
+                audioIndex = 2;
                 break;
             case SDL_CONTROLLER_BUTTON_DPAD_RIGHT:
-                arrowPressed = 3;
+                audioIndex = 3;
                 break;
         }
-        if (arrowPressed != -1){
+        if (audioIndex != -1){
             char command[100];
-            sprintf(command, "mpg123 %s &", audioFiles[arrowPressed]);  // mpg123 permet de lire fichier mp3 -> sprintf permet de formater une Cdc
+            sprintf(command, "mpg123 audio/audio%i.mp3 &", audioIndex);  // mpg123 permet de lire fichier mp3 -> sprintf permet de formater une Cdc
             system(command); // permet de lancer l'execution d'une commande
         }
     }
