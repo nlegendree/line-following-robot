10836bc29909e950f12b8e7fbdb54d1f765d3f16
angelo <angelo.bou_tanous@insa-rouen.fr>
2023-12-17 20:33:48 +0100

    speaker add digitalWrite

--- code/src/speaker.c
+++ code/src/speaker.c
@@ -10,6 +10,7 @@ void initSpeaker() {
     pinMode(PIN_SPEAKER, OUTPUT);
 }
 
+<<<<<<< HEAD
 void playAudio(SDL_Event event, char argv0[]) {
     char *audios[] = {"audio1.mp3","audio2.mp3","audio3.mp3","audio4.mp3"};
     int audioIndex = -1;
@@ -34,5 +35,36 @@ void playAudio(SDL_Event event, char argv0[]) {
             sprintf(command, "mpg123 %s/../audio/%s &", dirname(programPath), audios[audioIndex]);  // mpg123 permet de lire fichier mp3 -> sprintf permet de formater une Cdc
             system(command); // permet de lancer l'execution d'une commande
         }
+=======
+void playAudio(int button) {
+    const char *audioFiles[] = {"audio1.mp3", "audio2.mp3", "audio3.mp3", "audio4.mp3"};
+
+    // associe une valeur à la touche pressée par l'utilisateur
+    int arrowPressed = -1;
+    switch (button) {
+        case SDL_CONTROLLER_BUTTON_DPAD_UP:
+            arrowPressed = 1;
+            break;
+        case SDL_CONTROLLER_BUTTON_DPAD_DOWN:
+            arrowPressed = 2;
+            break;
+        case SDL_CONTROLLER_BUTTON_DPAD_LEFT:
+            arrowPressed = 3;
+            break;
+        case SDL_CONTROLLER_BUTTON_DPAD_RIGHT:
+            arrowPressed = 4;
+            break;
+        default:
+            return;
+    }
+    if (arrowPressed != -1){
+        digitalWrite(PIN_SPEAKER, HIGH);
+
+        const char command[100];
+        sprintf(command, "mpg123 %s", audioFiles[arrowPressed-1]);  // mpg123 permet de lire fichier mp3 -> sprintf permet de formater une Cdc
+        system(command) // permet de lancer l'execution d'une commande
+
+        digitalWrite(PIN_SPEAKER, LOW);
+>>>>>>> 969dd87 (speaker add digitalWrite)
     }
 }
