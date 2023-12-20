e6508aea53973bcdfd87e5922d5027c58e66b588
Angelo Bou Tanous <angelo.bou_tanous@insa-rouen.fr>
2023-12-20 15:26:48 +0100

    test nouveaux lineFinder

--- code/src/lineFinder.c
+++ code/src/lineFinder.c
@@ -28,10 +28,10 @@ void LF_forward() {
 }
 
 void LF_turnLeft() {
-    digitalWrite(PIN_M1A,HIGH);
-    digitalWrite(PIN_M2A,LOW);
-    digitalWrite(PIN_M1B,LOW);
-    digitalWrite(PIN_M2B,HIGH);
+    digitalWrite(PIN_M1A,LOW);
+    digitalWrite(PIN_M2A,HIGH);
+    digitalWrite(PIN_M1B,HIGH);
+    digitalWrite(PIN_M2B,LOW);
     
     float coeffSpeed = (float)LF_SPEED/MAX_TRIGGER;
     pwmWrite(PIN_EN1,(int)round(coeffSpeed*PWM_RANGE));
